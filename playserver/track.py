be86faea5dcab7a5c10169d12188a6091b6fd104
Nick Krichevsky <njk828@gmail.com>
2016-04-24 14:59:58 -0400

    Add executeCommand function

--- playserver/track.py
+++ playserver/track.py
@@ -1,7 +1,7 @@
 import configmanager
 import osascript
 
-APP_CONFIG_PATH = "applications/"
+APP_CONFIG_PATH = "../applications/"
 
 applicationConfigs = configmanager.ConfigManager(APP_CONFIG_PATH)
 #TODO: Make this user choosable
@@ -9,3 +9,11 @@ currentApplication = "radiant"
 
 def getCurrentConfig():
 	return applicationConfigs[currentApplication]
+
+def executeCommand(command):
+	config = getCurrentConfig()
+	fullCommand = "tell application \"{}\" to {}".format(config["name"],
+		config["commands"][command])
+	result = osascript.osascript(fullCommand)
+	#Return the return code of the command - 0 if succuessful.
+	return result[0]
