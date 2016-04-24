9de44f75bdf0c4cfa5b5841ba37f3fb32debb7ad
Nick Krichevsky <njk828@gmail.com>
2016-04-24 15:19:56 -0400

    Add functions for various player actions

--- playserver/track.py
+++ playserver/track.py
@@ -15,3 +15,36 @@ def _executeCommand(command):
 	fullCommand = "tell application \"{}\" to {}".format(config["name"],
 		config["commands"][command])
 	return osascript.osascript(fullCommand)
+
+def playPause():
+	_executeCommand("playpause")
+
+def next():
+	_executeCommand("next")
+
+def previous():
+	_executeCommand("previous")
+
+def getCurrentSong():
+	result = _executeCommand("currentsong")
+	#Return stdout if there is no error, stderr if otherwise
+	if result[0] == 0:
+		return result[1]
+	else:
+		return result[2]
+
+def getCurrentAlbum():
+	result = _executeCommand("currentalbum")
+	#Return stdout if there is no error, stderr if otherwise
+	if result[0] == 0:
+		return result[1]
+	else:
+		return result[2]
+
+def getCurrentArtist():
+	result = _executeCommand("currentartist")
+	#Return stdout if there is no error, stderr if otherwise
+	if result[0] == 0:
+		return result[1]
+	else:
+		return result[2]
