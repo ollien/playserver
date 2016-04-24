b3b281c9bf789c44a9a2b2d750e4dd8cf789dd1a
Nick Krichevsky <njk828@gmail.com>
2016-04-24 15:29:49 -0400

    Add song display to root page

--- playserver/webserver.py
+++ playserver/webserver.py
@@ -5,4 +5,7 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def root():
-	return "{} by {} - {}"
+	song = track.getCurrentSong()
+	artist = track.getCurrentArtist()
+	album = track.getCurrentAlbum()
+	return "{} by {} - {}".format(song, artist, album)
