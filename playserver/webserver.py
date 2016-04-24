70f0be172801ee5fd205a90c78e2bf66f8e4ae07
Nick Krichevsky <njk828@gmail.com>
2016-04-24 15:42:33 -0400

    Add basic routes for controls and song info

--- playserver/webserver.py
+++ playserver/webserver.py
@@ -1,4 +1,5 @@
 import flask
+import json
 from . import track
 
 app = flask.Flask(__name__)
@@ -9,3 +10,26 @@ def root():
 	artist = track.getCurrentArtist()
 	album = track.getCurrentAlbum()
 	return "{} by {} - {}".format(song, artist, album)
+
+@app.route("/get_song_info")
+def getSongInfo():
+	return json.dumps({
+		"name": track.getCurrentSong(),
+		"artist": track.getCurrentAritst(),
+		"album": track.getCrrentAlbum()
+	})
+
+@app.route("/play_pause", methods = ["POST"])
+def playPause():
+	track.playPause()
+	return ""
+
+@app.route("/next", methods = ["POST"])
+def next():
+	track.next()
+	return ""
+
+@app.route("/previous", methods = ["POST"])
+def previous():
+	track.previous()
+	return ""
