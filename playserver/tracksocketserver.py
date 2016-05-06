a691f6cecf2b8729dffb909e1536b9a6fdf09a9b
Nick Krichevsky <njk828@gmail.com>
2016-05-06 16:11:37 -0400

    Add TrackSocketServerFactory

--- playserver/tracksocketserver.py
+++ playserver/tracksocketserver.py
@@ -10,3 +10,12 @@ class TrackSocket(autobah.asyncio.websocket.WebSocketServerProtocol):
 	def closed(self, wasClean, code, reason):
 		#TODO: make this remove from the client list
 		print("closed")
+
+class TrackSocketServerFactory(autobah.asyncio.websocket.WebSocketServerFactory):
+	def __init__(self, url):
+		super().__init__(self, url)
+		self.clients = []
+	
+	def broadcastMessage(self, message):
+		for client in selfclients:
+			client.sendMessage(message)
