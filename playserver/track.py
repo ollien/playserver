c3d41330759f64e9ba010a6c0709e5c3682649d0
Nick Krichevsky <njk828@gmail.com>
2016-05-08 12:21:08 -0400

    Convert track.py into a class

--- playserver/track.py
+++ playserver/track.py
@@ -3,64 +3,68 @@ from . import configmanager
 
 APP_CONFIG_PATH = "applications/"
 
-applicationConfigs = configmanager.ConfigManager(APP_CONFIG_PATH)
-currentApplication = "radiant"
+controller = TrackController()
 
-def setApplication(name):
-	global currentApplication
-	currentApplication = name
+class _TrackController():
+	
+	def __init__(self, application = "radiant")
+		self.currentApplication = application
+		self._applicationConfigs = configmanager.ConfigManager(APP_CONFIG_PATH)
 
-def getAvailableApplications():
-	return list(applicationConfigs._configs.keys())
+	def setApplication(self, name):
+		self.currentApplication = name
 
-def getCurrentConfig():
-	return applicationConfigs[currentApplication]
+	def getAvailableApplications(self):
+		return list(self._applicationConfigs._configs.keys())
 
-def _executeCommand(command):
-	config = getCurrentConfig()
-	fullCommand = "tell application \"{}\" to {}".format(config["name"],
-		config["commands"][command])
-	return osascript.osascript(fullCommand)
+	def getCurrentConfig(self):
+		return self._applicationConfigs[currentApplication]
 
-def isPlaying():
-	playerStates = getCurrentConfig()["player-states"]
-	result = _executeCommand("state")
+	def _executeCommand(self, command):
+		config = self.getCurrentConfig()
+		fullCommand = "tell application \"{}\" to {}".format(config["name"],
+			config["commands"][command])
+		return osascript.osascript(fullCommand)
 
-	if (result[1] == playerStates["stopped"] or
-		result[1] == playerStates["paused"]):
-		return False
-	else:
-		return True
+	def isPlaying(self):
+		playerStates = self.getCurrentConfig()["player-states"]
+		result = self._executeCommand("state")
 
-def playPause():
-	_executeCommand("playpause")
+		if (result[1] == playerStates["stopped"] or
+			result[1] == playerStates["paused"]):
+			return False
+		else:
+			return True
 
-def next():
-	_executeCommand("next")
+	def playPause(self):
+		self._executeCommand("playpause")
 
-def previous():
-	_executeCommand("previous")
+	def next(self):
+		self._executeCommand("next")
 
-def getCurrentSong():
-	result = _executeCommand("currentsong")
-	#Return stdout if there is no error, stderr if otherwise
-	if result[0] == 0:
-		return result[1]
-	else:
-		return result[2]
+	def previous(self):
+		self._executeCommand("previous")
 
-def getCurrentAlbum():
-	result = _executeCommand("currentalbum")
-	#Return stdout if there is no error, stderr if otherwise
-	if result[0] == 0:
-		return result[1]
-	else:
-		return result[2]
+	def getCurrentSong(self):
+		result = self._executeCommand("currentsong")
+		#Return stdout if there is no error, stderr if otherwise
+		if result[0] == 0:
+			return result[1]
+		else:
+			return result[2]
 
-def getCurrentArtist():
-	result = _executeCommand("currentartist")
-	#Return stdout if there is no error, stderr if otherwise
-	if result[0] == 0:
-		return result[1]
-	else:
-		return result[2]
+	def getCurrentAlbum(self):
+		result = self._executeCommand("currentalbum")
+		#Return stdout if there is no error, stderr if otherwise
+		if result[0] == 0:
+			return result[1]
+		else:
+			return result[2]
+
+	def getCurrentArtist():
+		result = self._executeCommand("currentartist")
+		#Return stdout if there is no error, stderr if otherwise
+		if result[0] == 0:
+			return result[1]
+		else:
+			return result[2]
